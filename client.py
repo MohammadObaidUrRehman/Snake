dc85c7ca28e8fe5c1519829d9858b5fd632e4375
Mohammad Obaid Ur Rehman <38375574+MohammadObaidUrRehman@users.noreply.github.com>
2019-03-18 02:35:41 +0500

    Iteration2

--- client.py
+++ client.py
@@ -1,59 +1,165 @@
-import socket
+import socket # communication
 import sys
-from _thread import *
-import random
-import curses
+from thread import *# threading concurrency and parallelism 
+import random # random number generation 
+import curses # game related features e.g rendering
+import ast # sting to array conversion
 
-try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error:
-	print ("Failed to connect")
-	sys.exit()
-
-print("socket created")
-host = "localhost"
-port = 8888
-
-try:
-	remote_ip = socket.gethostbyname(host)
-except socket.gaierror:
-	print("host name could not be resolved")
-	sys.exit();
-
-print("IP addresss: "+ remote_ip)
-
-s.connect((remote_ip,port))
-
-print("socket connected to " + host + " using IP " + remote_ip)
-
-# def listener(socket_var):
-# 	while 1:
-# 		reply = s.recv(4096)
-# 		print(reply.decode())
-
-# def speaker(socket_var):
-# 	while 1:	
-# 		message = input("enter your message	")
-# 		try:
-# 			s.sendall(message.encode())
-# 		except socket.error:
-# 			print("did not send successfully")
-# 			sys.exit()
-# 		print ("message send successfully ")
-
-# start_new_thread(listener, (s,))
-# start_new_thread(speaker,  (s,))
-while 1:
-	# message = "hello what is your name"
-	message = input("enter your message	")
+# function to send data across the socket
+# sock is the connection
+# message is the string we need to send
+def send_using_socket(sock,message):
 	try:
-		s.sendall(message.encode())
+		# always need to encode before sending
+		sock.sendall(message.encode())
 	except socket.error:
 		print("did not send successfully")
 		sys.exit()
-	print ("message send successfully ")
 
-	reply = s.recv(4096)
-	print(reply.decode())
+# this is where the programs starts from 
+# initialisation of socket
+try:
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+	print ("Failed to connect")
+	sys.exit()
+# define port and host 
+ack = "ack"
+host = "localhost"
+port = 8888
+# resolve host "localhost" in our case
+try:
+	remote_ip = socket.gethostbyname(host)
+except socket.gaierror:
+	# print("host name could not be resolved")
+	sys.exit();
 
-s.close()
+sock.connect((remote_ip, port))
+
+tempdim = sock.recv(1024) # recieve dimensions of the screen
+sock.send(ack.encode())
+dim = ast.literal_eval(tempdim.decode())
+# decode dimensions of the screen
+sh = dim[0][0] # height of the screen (y dimension)
+sw = dim[0][1] # width of the screen  (x dimension)
+# initialize new window with height and width
+stdscr = curses.initscr()
+curses.curs_set(0)
+w = curses.newwin(int(sh), int(sw), 0, 0)
+w.box() # create border around the player screen
+w.keypad(1) # intialize to get keyboard input
+w.timeout(1000) # set game speed, higher value means slower game
+snakeTemp = sock.recv(1024) # recieve snake dimensions
+sock.send(ack.encode())
+
+snakeTemp = snakeTemp.decode() # decode string into array
+snake = ast.literal_eval(snakeTemp)
+
+myPlayNum = sock.recv(1024)
+sock.send(ack.encode())
+
+myPlayNum = ast.literal_eval(myPlayNum.decode())
+mySnake = snake[myPlayNum]
+food = [sh/2, sw/2]
+
+w.addch(food[0], food[1], curses.ACS_PI)
+key = curses.KEY_RIGHT # intially the input is hardcoded to right
+# infinite while loop
+while 1:
+	# function to get keyboard input 
+	next_key = w.getch()
+	key = key if next_key == -1 else next_key
+	# ending conditions
+	# if the snak reaches the border then end game
+	# if the snake eats himeself them end game
+	# print "before ending", mySnake
+	if mySnake[0][0] in [0, sh] or mySnake[0][1]  in [0, sw] or mySnake[0] in mySnake[1:]:
+		curses.endwin()
+		quit()
+	# print "2"]
+	new_head = [mySnake[0][0], mySnake[0][1]]
+# encode the action to be taken in the string, and then send it to socket
+# for now the socket just replies back with the same message
+# we need to add functionality and in the sockets for this
+	if key == curses.KEY_DOWN:
+		message="down"
+	if key == curses.KEY_UP:
+		message="up"
+	if key == curses.KEY_LEFT:
+		message="left"
+	if key == curses.KEY_RIGHT:
+		message="right"
+	# print "3"
+	temp = str([message] + mySnake) 
+	# print temp
+	sock.recv(1024) #1 wait for ack
+	sock.send(temp.encode()) #2 send input
+	reply = sock.recv(1024) #3 recv validated input
+	resp = ast.literal_eval(reply.decode())
+	sock.send(ack.encode()) #4
+	tFood = sock.recv(1024) #5
+	sock.send(ack.encode()) #6
+	# print "4"
+	
+# move the mySnake according to keyboard input 
+	response = str(resp[0])
+	# print response
+	if response == "down":
+		new_head[0] += 1
+	if response == "up":
+		new_head[0] -= 1
+	if response == "left":
+		new_head[1] -= 1
+	if response == "right":
+		new_head[1] += 1
+	# print "5"
+
+# we pop from the tail and add to the head to make it seem like the mySnake is moving
+# if the mySnake eats food we don't pop from the tails to make it look like the mySnake has grown
+	# print new_head
+	mySnake.insert(0, new_head)
+	# print "6"
+
+	# this code has been commented for now, it refreshes the food whenever the mySnake eats food
+	# with this code the mySnake also grows when it eats
+	# all this code needs to be added to the server
+	""" 
+	# if mySnake[0] == food:
+	# 	food = None
+	# 	while food is None:
+	# 		nf = [
+	# 			random.randint(1, sh-1),
+	# 			random.randint(1, sw-1)
+	# 		]
+	# 		food = nf if nf not in mySnake else None
+	# w.addch(food[0], food[1], curses.ACS_PI)
+	# else:
+		# tail = mySnake.pop()
+	# 	w.addch(tail[0], tail[1], ' ')
+"""
+	# in the current implemtntaion the mySnake doesn't grow
+			# w.addch(food[1], food[0], ' ')
+			# food = ast.literal_eval(tFood.decode())
+			# w.addch(food[1], food[0], curses.ACS_PI)
+# render new food on the screen
+
+
+	tail = mySnake.pop()
+	w.addch(int(tail[0]), int(tail[1]), ' ')
+	# print "snake ", snake[0][0]
+	w.addch(int(mySnake[0][0]), int(mySnake[0][1]), curses.ACS_CKBOARD)
+	# print tail
+	# print "7"
+
+	# w.addch(int(tail[1]), int(tail[0]), ' ')
+
+	# w.addch(int(tail[0]), int(tail[1]), ' ')
+# pop from the tail
+	# print mySnake
+	# w.addch(int(mySnake[0][0]), int(mySnake[0][1]), curses.ACS_CKBOARD)
+	# print "1"
+# add to the head
+# sock.close()
+
+
+# need to deal with tail error maybe array is going our of bounds
--- server.py
+++ server.py
@@ -1,44 +1,139 @@
-import socket
+from _thread import * # each player will have a seperate thread
+import socket # communication between clients via server (sending and recieving data)
 import sys
-from _thread import *
-
-
+import random # generate random numbers
+import time # use the time.sleep function
+import ast # convert string to array
+# port number and local host can change this aswell
+# local host
 host = ''
 port = 8888
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ack = "ack"
+play_n = 0 # number of players in the game
+ydim = 20 # y dimension of the screen aka height
+xdim = 50 # x dimension of the screen aka width
+food_pos = [ydim/2,xdim/2,"food"] # initial postiton of the first snake food
+
+# only head coordinates for the first 4 snakes
+# at the moment only catering for snakes can expand later 
+snakes = [
+[[  (ydim/5), xdim/10],[  ((ydim/5)-1), xdim   ],[  ((ydim/5)-2), xdim   ]],
+[[(2*ydim/5), xdim/10],[((2*ydim/5)-1), xdim/10],[((2*ydim/5)-2), xdim/10]],
+[[(3*ydim/5), xdim/10],[((3*ydim/5)-1), xdim/10],[((3*ydim/5)-2), xdim/10]],
+[[(4*ydim/5), xdim/10],[((4*ydim/5)-1), xdim/10],[((4*ydim/5)-2), xdim/10]]
+]
+
+# create socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("socket created")
 
+# bind socker to host and port
 try:
-	s.bind((host, port))
+	sock.bind((host, port))
 except socket.error:
 	print ("binding failed")
 	sys.exit()
 
 print("socket has been bounded")
 
-s.listen(10)
+sock.listen(10) # listen for a max of 10 connections
 
 print("socket is ready to listen")
 
-# message = input("enter your message	")
-def clientthread(conn):
-	counter=0
-	message = "welcome to the server. type something and hit enter"
+# this function launches whenever a player(client) connect
+def clientthread(conn,count):
+	# height of the screen
+	global ydim
+	# widht of the screen
+	global xdim
+	# position of food
+	global food_pos
+	# number of players
+	global play_n
+	# put all the data in a an array
+	temp = [[ydim]+[xdim]]
+	# temp = [[ydim]+[xdim]+["dim"]]+[food_pos]
+	# convert that array data to a string
+	message = str(temp)
+	print(message)
+	print(temp) 
+	# send the data using a socket
 	conn.send(message.encode())
+	conn.recv(1024) # wait for ack
+
+	temp = ""
+	temp = str(snakes)
+	print(temp)
+	conn.send(temp.encode())
+	conn.recv(1024) #wait for ack
+	
+	msg = str(play_n)
+	conn.send(msg.encode())
+	conn.recv(1024) #wait for ack
+	play_n = play_n+1
+
 	while 1:
-		data = conn.recv(1024)
-		reply = "OK" + data.decode()
+		# reciece data from the player
+		print("sdare ")
+
+		conn.send(ack.encode()) #1 "up down left right"
+		print("data ")		
+		data = conn.recv(1024) #2
+		print("ata ")
 		if not data:
 			break;
-		print(reply)
-		sendData = "message from server number" + str(counter) 
-		conn.send(sendData.encode())
-		counter=counter+1
+		# error handling
+		conn.send(data) #3 send the data that to the clients
+		conn.recv(1024) #4
+		print("sdre ")
+
+		tempFood=str(food_pos)
+		print("food")
+		conn.send(tempFood.encode()) #5
+		print("tempfood")
+		conn.recv(1024) #6
+		print("here ")
 	conn.close()
 
+# this function runs in parrallel with all the threads and generates foods randomly across the screen
+# conn is the socket connection data and food_present is a boolenan variable 
+# food present tells us whether the food is present on the grid or not
+# for now food_present is not being used in this code
+def food_function(dummy_var,food_present):
+	global xdim
+	global ydim
+	global food_pos
+	while 1:
+		# while food_present==0: do this at a later stage
+		time.sleep(3) # sleeps for 3 seconds and then generates a new food item.
+		food_pos = [
+			random.randint(1, ydim-1),
+			random.randint(1, xdim-1),
+		]
+
+user_list = [] # to maintain a list of all users
+conn_list = [] # to maintain a list of all connections
+start_new_thread(food_function,("dummy",0))
+print( "the snake is ", snakes)
 while 1:
-	conn, addr = s.accept()
-	print("connected with" +  addr[0] + ":" + str(addr[1]))
-	start_new_thread(clientthread, (conn,))
-s.close()
+	conn, addr = sock.accept()
+	# accept connections when a client tries to connect
+	print("connected with " +  addr[0] + ":" + str(addr[1]))
+	user_list.append(addr[1])
+	conn_list.append(conn)
+	start_new_thread(clientthread, (conn,0))
+sock.close()
+# close socket
+
+# to-do
+# food should be same for both 
+# generate random variable for snake position?
+# loosing condition 
+# growing condition
+# wait for players before starting
+
+# main condition
+# show all snakes on all screen
+
+# how to run between computers?
